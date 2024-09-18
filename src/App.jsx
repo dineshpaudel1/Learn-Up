03cde5be9573300edcc87af12a58e8b24e311806
Dinesh Paudel <paudeldinesh961@gmail.com>
2024-09-18 22:59:37 +0545

    changes happen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./UserLayout";
 import Home from "./user/Page/Home";
 import Login from "./user/login/Login";
@@ -8,17 +8,14 @@ import Signup from "./user/Signup/Signup";
 import Profile from "./user/Page/Profile";
 import { UserInfoProvider } from "./user/context/UserInfoProvider";
 import AdminLayout from "./AdminLayout";
-import Dashboard from "./admin/Pages/Dashboard";
-import DHome from "./admin/Pages/DHome";
+import Dashboard from "./admin/Dashobard/Dashboard";
 import Coursedetail from "./user/Page/Coursedetail";
-
+import CourseAdmin from "./admin/CourseAdmin/CourseAdmin";
 function App() {
   return (
     <>
       <UserInfoProvider>
-        <Router>
-          {" "}
-          {/* Add the Router here */}
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<UserLayout />}>
               <Route index element={<Home />} />
@@ -28,12 +25,12 @@ function App() {
               <Route path="/coursedetail" element={<Coursedetail />} />
             </Route>
             <Route path="/admin" element={<AdminLayout />}>
-              <Route path="good" element={<DHome />} />
               <Route index element={<Dashboard />} />
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="courseadmin" element={<CourseAdmin />} />
             </Route>
           </Routes>
-        </Router>{" "}
-        {/* Close the Router here */}
+        </BrowserRouter>
       </UserInfoProvider>
     </>
   );
